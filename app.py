13edd3f1a041b4260a8ccd08591550e80c5738b1
Matthew Jeng <mjeng@berkeley.edu>
2018-05-21 17:26:07 -0400

    (MILESTONE) TRY ONCE WORKS WOOOOOO. I GOT A TEXT USING TRY ONCE. IT DISPLAYS THE RESPONSE PAGE TOO. WOOOOOOO Modified app and router. app now is compatible with requests, instead of always returning the default page. Added run_once to router! Manually defined authentication keys in file - will port to config vars later.

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, request, render_template
+import server_utils
+import router
 
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def homepage():
-    if request.method == "POST":
-        pass # TODO: to be implemented
     print(request.method)
     print(request.form)
+
+    if request.method == "GET":
+        return render_template("index.html")
+
+    if request.method == "POST":
+        name = request.form["name"]
+        number = request.form["phone"]
+        timestr = server_utils.parse_validate_time(request.form["time"])
+        submit_type = request.form["submit_type"]
+
+        if submit_type == server_utils.TRY:
+            router.run_once(name, number)
+            return render_template("try.html", number=number)
+        if submit_type == server_utils.SUB:
+            router.add_db_entry(name, number, timestr)
+            return render_template("sub.html", name=name)
+
     return render_template("index.html")
 
 
--- router.py
+++ router.py
@@ -2,8 +2,32 @@ import os
 import twilio_utils, scrape_utils, db_utils
 import random
 
+# TODO: temporary; replace with heroku config
+f = open("private.txt", 'r')
+account_sid = f.readline()[:-1] # to get rid of \n
+auth_token = f.readline()[:-1]
+send_to = f.readline()[:-1]
+send_from = f.readline()[:-1]
+f.close()
+
 def add_db_entry(name, number, timestr):
     db_utils.add_entry(name, number, timestr)
+
+def run_once(name, number):
+
+    mrcn = scrape_utils.most_recent_comic_num()
+    comic_num = random.randint(1, mrcn)
+
+    mms = twilio_utils.MMS(name, number, comic_num)
+
+    comic_url = scrape_utils.find_comic_url(comic_num)
+    caption = scrape_utils.find_comic_caption(comic_num)
+
+    mms.update(comic_url, caption)
+
+    twilio_client = twilio_utils.ClientWrapper(account_sid, auth_token, send_from)
+    twilio_client.send_mms(mms)
+
 
 # NOTE: runs periodically vis-a-vis heroku scheduler
 def run():
