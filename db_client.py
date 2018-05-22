e3920551dfa57df153b446d9fe9f1d067fdeafce
Matthew Jeng <mjeng@berkeley.edu>
2018-05-22 03:58:53 -0400

    Changed GOOGLE_AUTH config var. Holy fuck that took a long time for like 2 changes.

--- db_client.py
+++ db_client.py
@@ -5,13 +5,13 @@ from google.auth.transport.requests import AuthorizedSession
 
 def setup_db():
     global wb
-    _service_account_info = os.environ["GOOGLE_AUTH"]
+    _service_account_info = json.loads(os.environ["GOOGLE_AUTH"])
     _scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     _credentials = service_account.Credentials.from_service_account_info(_service_account_info)
     _scoped_credentials = _credentials.with_scopes(_scope)
     _spreadsheetId = os.environ["WB_ID"]
     _gc = gspread.Client(auth=_scoped_credentials)
     _gc.session = AuthorizedSession(_scoped_credentials)
-    wb = _gc.open_by_key(spreadsheetId)
+    wb = _gc.open_by_key(_spreadsheetId)
 
 setup_db()
