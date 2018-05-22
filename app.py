bbd0b3c0836d1030dec014bf66e5eafb8f3120b1
Matthew Jeng <mjeng@berkeley.edu>
2018-05-21 21:59:01 -0400

    (major) Added vertically integrated server-side input validation. This includes an error page with a error-specific message as well.

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, request, render_template
 import server_utils
 import router
 
+###############################################
+SERVER_ERR_MSGS = {
+    server_utils.NAME_ERR: "your name input is too long. The character limit is 100.",
+    server_utils.NUM_ERR: "the phone number you input isn't valid.",
+    server_utils.TIME_ERR: "your time input was corrupted somehow. Refreshing might help.",
+    server_utils.SUBMIT_ERR: "your submission was corrupted, somehow. Refreshing might help.",
+}
+###############################################
+
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def homepage():
@@ -15,9 +24,18 @@ def homepage():
         name = request.form["name"]
         number = request.form["phone"]
         time = request.form["time"]
-        timestr = server_utils.parse_validate_time(time)
         submit_type = request.form["submit_type"]
 
+        twilio_client = router.twilio_client
+        err_code = server_utils.validate_inputs(name, number, twilio_client, time, submit_type)
+
+        if err_code == server_utils.NO_ERR:
+            pass
+        else:
+            err_msg = SERVER_ERR_MSGS[err_code]
+            return render_template("err.html", err_msg=err_msg)
+
+        timestr = server_utils.parse_time(time)
         if submit_type == server_utils.TRY:
             router.run_once(name, number)
             return render_template("try.html", number=number)
--- server_utils.py
+++ server_utils.py
@@ -1,4 +1,5 @@
 import re
+import twilio.base.exceptions
 
 ###############################################
 TRY = "try it"
@@ -10,11 +11,18 @@ HOURS = [str(i+1).zfill(2) for i in range(12)]
 MINUTES = ['00', '30']
 AM = 'AM'
 PM = 'PM'
+
+MAX_NAME_LENGTH = 100
+NO_ERR = 0
+NAME_ERR = 1
+NUM_ERR = 2
+TIME_ERR = 3
+SUBMIT_ERR = 4
 ###############################################
 
 # NOTE: turns out you can change the values of the select box before submission
 #   so validating to make sure it's one of the options provided
-def parse_validate_time(time):
+def parse_time(time):
     """
     Returns string denoting the time with format 'hhmm'. Function makes sure the
     selected option is one of the provided ones.
@@ -25,6 +33,8 @@ def parse_validate_time(time):
     >>> parse_validate_time("6:31 PM")
     AssertionError
     """
+
+    # NOTE: validation not necessary here anymore but just kept it ¯\_(ツ)_/¯
 
     res = re.findall(TIME_FORMAT, time.zfill(FORMAT_LENGTH))
     assert len(res) == 1, "Issue with time format"
@@ -44,3 +54,58 @@ def parse_validate_time(time):
         hours = str(int(hours) + 12)
 
     return hours + minutes
+
+def validate_name(name):
+    try:
+        assert isinstance(name, str)
+        assert len(name) <= MAX_NAME_LENGTH
+    except AssertionError:
+        return False
+    return True
+
+def validate_number(number, twilio_client):
+    pnc = twilio_client.client.lookups.phone_numbers(number)
+    try:
+        pn = pnc.fetch()
+        num = pn.phone_number
+    except twilio.base.exceptions.TwilioRestException:
+        return False
+    return True
+
+def validate_time(time):
+    res = re.findall(TIME_FORMAT, time.zfill(FORMAT_LENGTH))
+    try:
+        assert len(res) == 1, "Issue with time format"
+    except AssertionError:
+        return False
+
+    raw = res[0]
+    hours = raw[0:2]
+    minutes = raw[3:5]
+    ampm = raw[6:8]
+
+    try:
+        assert hours in HOURS, "Corrupted input"
+        assert minutes in MINUTES, "Corrupted input"
+        assert ampm in [AM, PM], "Corrupted input"
+    except AssertionError:
+        return False
+    return True
+
+def validate_submit_type(submit_type):
+    try:
+        assert submit_type == TRY or submit_type == SUB
+    except AssertionError:
+        return False
+    return True
+
+def validate_inputs(name, number, twilio_client, time, submit_type):
+    if not validate_name(name):
+        return NAME_ERR
+    if not validate_number(number, twilio_client):
+        return NUM_ERR
+    if not validate_time(time):
+        return TIME_ERR
+    if not validate_submit_type(submit_type):
+        return SUBMIT_ERR
+    return NO_ERR
